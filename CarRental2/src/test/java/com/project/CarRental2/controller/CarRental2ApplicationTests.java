676a5b4cb8621b0b84eb7361e6f211ac2d8ff891
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-01 22:11:37 +0700

    10h11

--- CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
@@ -1,74 +1,21 @@
 package com.project.CarRental2.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; // Sử dụng annotation @Test từ JUnit 5
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.project.CarRental2.CarRental2Application;
-import com.project.CarRental2.service.BlogService;
-import com.project.CarRental2.service.BookingService;
-import com.project.CarRental2.service.CarService;
-import com.project.CarRental2.service.ContractService;
-import com.project.CarRental2.service.DetailNotificationService;
-import com.project.CarRental2.service.DistrictService;
-import com.project.CarRental2.service.InsuranceService;
-import com.project.CarRental2.service.NotificationService;
-import com.project.CarRental2.service.ProvinceService;
-import com.project.CarRental2.service.RequestWithdrawalService;
-import com.project.CarRental2.service.UserService;
-
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(HomePageController.class)
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})  // Loại bỏ Security và JPA auto-config
 class HomePageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    // Mock tất cả service để tránh lỗi khởi tạo
-    @MockBean
-    private UserService userService;
-
-    @MockBean
-    private ProvinceService provinceService;
-
-    @MockBean
-    private CarService carService;
-
-    @MockBean
-    private DistrictService districtService;
-
-    @MockBean
-    private BookingService bookingService;
-
-    @MockBean
-    private BlogService blogService;
-
-    @MockBean
-    private InsuranceService insuranceService;
-
-    @MockBean
-    private ContractService contractService;
-
-    @MockBean
-    private NotificationService notificationService;
-
-    @MockBean
-    private DetailNotificationService detailNotificationService;
-
-    @MockBean
-    private RequestWithdrawalService requestWithdrawalService;
-
-    @Test
+    @Test // Annotation @Test để đánh dấu phương thức test
     void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
