0464672e0b299c3fb3b8e5a0185a6fd91fd48efd
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-01 21:53:29 +0700

    9h53

--- CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
@@ -3,17 +3,67 @@ package com.project.CarRental2.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.project.CarRental2.CarRental2Application;
+import com.project.CarRental2.service.BlogService;
+import com.project.CarRental2.service.BookingService;
+import com.project.CarRental2.service.CarService;
+import com.project.CarRental2.service.ContractService;
+import com.project.CarRental2.service.DetailNotificationService;
+import com.project.CarRental2.service.DistrictService;
+import com.project.CarRental2.service.InsuranceService;
+import com.project.CarRental2.service.NotificationService;
+import com.project.CarRental2.service.ProvinceService;
+import com.project.CarRental2.service.RequestWithdrawalService;
+import com.project.CarRental2.service.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(HomePageController.class)
+@Import(CarRental2Application.class)  // Chỉ định class chính để khởi tạo context cơ bản
 class HomePageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // Mock tất cả các service được autowired trong controller
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private ProvinceService provinceService;
+
+    @MockBean
+    private CarService carService;
+
+    @MockBean
+    private DistrictService districtService;
+
+    @MockBean
+    private BookingService bookingService;
+
+    @MockBean
+    private BlogService blogService;
+
+    @MockBean
+    private InsuranceService insuranceService;
+
+    @MockBean
+    private ContractService contractService;
+
+    @MockBean
+    private NotificationService notificationService;
+
+    @MockBean
+    private DetailNotificationService detailNotificationService;
+
+    @MockBean
+    private RequestWithdrawalService requestWithdrawalService;
 
     @Test
     void testHomePage() throws Exception {
