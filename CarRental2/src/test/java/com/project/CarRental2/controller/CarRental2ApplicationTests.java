b317023b57a1086488f99c6534d01f68de37a314
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-01 21:48:15 +0700

    9h48

--- CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
@@ -2,20 +2,14 @@ package com.project.CarRental2.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.project.CarRental2.CarRental2Application;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@SpringBootTest(classes = CarRental2Application.class)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")  // Kích hoạt profile test để sử dụng H2
+@WebMvcTest(HomePageController.class)
 class HomePageControllerTest {
 
     @Autowired
