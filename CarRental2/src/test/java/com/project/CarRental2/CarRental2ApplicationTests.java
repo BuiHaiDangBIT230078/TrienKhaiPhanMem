853bae055288968f1bcefe6b0eeb3fd454a5e0bf
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-01 21:21:05 +0700

    9h21

--- CarRental2/src/test/java/com/project/CarRental2/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/CarRental2ApplicationTests.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.project.CarRental2.CarRental2Application;  // Import class chính
 import com.project.CarRental2.controller.HomePageController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(HomePageController.class)
+@WebMvcTest(value = HomePageController.class, controllers = CarRental2Application.class)  // Chỉ định class chính
 class HomePageControllerTest {
 
     @Autowired
