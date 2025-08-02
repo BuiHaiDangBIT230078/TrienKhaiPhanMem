47c103c3368017dd9a98a650ca25b966a43cdb50
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-02 18:37:55 +0700

    6h37

--- CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
@@ -1,11 +1,21 @@
 package com.project.CarRental2.controller;
 
+import com.project.CarRental2.model.Car;
+import com.project.CarRental2.model.DetailNotification;
+import com.project.CarRental2.model.Insurance;
+import com.project.CarRental2.model.Province;
+import com.project.CarRental2.service.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,42 +25,57 @@ class HomePageControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    // Mock TẤT CẢ service trong HomePageController
+    // Mock tất cả service trong HomePageController
     @MockBean
-    private com.project.CarRental2.service.UserService userService;
+    private UserService userService;
 
     @MockBean
-    private com.project.CarRental2.service.ProvinceService provinceService;
+    private ProvinceService provinceService;
 
     @MockBean
-    private com.project.CarRental2.service.CarService carService;
+    private CarService carService;
 
     @MockBean
-    private com.project.CarRental2.service.DistrictService districtService;
+    private DistrictService districtService;
 
     @MockBean
-    private com.project.CarRental2.service.BookingService bookingService;
+    private BookingService bookingService;
 
     @MockBean
-    private com.project.CarRental2.service.BlogService blogService;
+    private BlogService blogService;
 
     @MockBean
-    private com.project.CarRental2.service.InsuranceService insuranceService;
+    private InsuranceService insuranceService;
 
     @MockBean
-    private com.project.CarRental2.service.ContractService contractService;
+    private ContractService contractService;
 
     @MockBean
-    private com.project.CarRental2.service.NotificationService notificationService;
+    private NotificationService notificationService;
 
     @MockBean
-    private com.project.CarRental2.service.DetailNotificationService detailNotificationService;
+    private DetailNotificationService detailNotificationService;
 
     @MockBean
-    private com.project.CarRental2.service.RequestWithdrawalService requestWithdrawalService;
+    private RequestWithdrawalService requestWithdrawalService;
 
     @Test
     void testHomePage() throws Exception {
+        // Mock insuranceService để tránh IndexOutOfBoundsException
+        List<Insurance> insurances = new ArrayList<>();
+        Insurance insurance = new Insurance();
+        insurance.setContentInsurance("Nội dung bảo hiểm giả lập");
+        insurances.add(insurance);  // Thêm ít nhất 1 phần tử
+        when(insuranceService.getAllInsurance()).thenReturn(insurances);
+
+        // Mock các service khác để tránh lỗi khác
+        when(provinceService.getAllProvinceOrderByName()).thenReturn(new ArrayList<Province>());
+        // Sửa mock cho carService: dùng boolean true/false cho driver, int cho status
+        when(carService.getAllCarByDriverAndStatusCarOderByName(true, 1)).thenReturn(new ArrayList<Car>());  // HAS_DRIVERS (true)
+        when(carService.getAllCarByDriverAndStatusCarOderByName(false, 1)).thenReturn(new ArrayList<Car>()); // NO_DRIVERS (false)
+        when(detailNotificationService.getListDetailNotification(0)).thenReturn(new ArrayList<DetailNotification>());
+
+        // Thực hiện request GET /
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
