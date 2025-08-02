03521e65a60e262642618b070108b390f4e86845
BuiHaiDangBIT230078 <buihaidang31@gmail.com>
2025-08-02 18:33:54 +0700

    6h33

--- CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
+++ CarRental2/src/test/java/com/project/CarRental2/controller/CarRental2ApplicationTests.java
@@ -15,7 +15,10 @@ class HomePageControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    // Mock chỉ các service cần thiết cho endpoint GET /
+    // Mock TẤT CẢ service trong HomePageController
+    @MockBean
+    private com.project.CarRental2.service.UserService userService;
+
     @MockBean
     private com.project.CarRental2.service.ProvinceService provinceService;
 
@@ -23,14 +26,31 @@ class HomePageControllerTest {
     private com.project.CarRental2.service.CarService carService;
 
     @MockBean
+    private com.project.CarRental2.service.DistrictService districtService;
+
+    @MockBean
+    private com.project.CarRental2.service.BookingService bookingService;
+
+    @MockBean
+    private com.project.CarRental2.service.BlogService blogService;
+
+    @MockBean
     private com.project.CarRental2.service.InsuranceService insuranceService;
+
+    @MockBean
+    private com.project.CarRental2.service.ContractService contractService;
+
+    @MockBean
+    private com.project.CarRental2.service.NotificationService notificationService;
 
     @MockBean
     private com.project.CarRental2.service.DetailNotificationService detailNotificationService;
 
+    @MockBean
+    private com.project.CarRental2.service.RequestWithdrawalService requestWithdrawalService;
+
     @Test
     void testHomePage() throws Exception {
-        // Gửi request GET /
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
